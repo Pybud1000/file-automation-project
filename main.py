5365d39bb401881ff4464a162dafde65390c3dc3
Miguel Perlado <perladomiguel7@gmail.com>
2026-08-11 13:51:40 +0800

    v2, core functionality plus refactoring

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 from pathlib import Path
 import shutil
+import configparser
 
-#Input directory (where the files will be coming from)
-dir_path = Path(r"C:\Users\PCXPC\Documents\super secret hehehe\Proj\file-automation-project\Source")
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-#Output directory (where the unique folders will be created)
-output_path = Path(r"C:\Users\PCXPC\Documents\super secret hehehe\Proj\file-automation-project\Output")
+def load_matrix():
+    matrix = {}
+    with open("matrix.csv") as File:
+        for line in File:
+            line = line.strip()
+            parts = line.split(";")
+            if parts[0] == "extension":
+                continue
+            matrix[parts[0]] = parts[1]
+    return matrix
 
-# Initial loop to check the unique file types and create the right folders accordingly
+dir_path = Path(config["IO folders"]['source'])
+output_path = Path(config["IO folders"]['output'])
+
+keywords = load_matrix()
+
 for file in dir_path.iterdir():
 
-    entry = str(file) # Stringify the file name
-    name, extension = entry.split(".") # split the file name by the "." and store in name and extension
+    entry = str(file) 
+    ext = entry.split(".") 
 
-    folder_name = "" # initialize empty dynamic folder_name placeholder
+    folder_name = "" 
 
-    # Assign the Folder Name according to file extension
-    if extension == "csv":
-        folder_name = "csv" 
-    elif extension == "xlsx":
-        folder_name = "excel" 
-    elif extension == "txt": 
-        folder_name = "Text Files" 
-    elif extension == "jpg" or extension == "jpeg" or extension == "png":
-        folder_name = "Photos"
+    if ext[-1] in keywords:
+        folder_name = keywords[ext[-1]]
 
-    folder_path = output_path/folder_name # create the folder path pointing towards the output folder
-    folder_path.mkdir(parents=True, exist_ok=True) # create the folder, bypass if already exists
+    folder_path = output_path/folder_name
+    folder_path.mkdir(parents=True, exist_ok=True)
     shutil.copy2(file, folder_path)
 
 
