15e423618da4a17cc5db23869f2fcd32b74b93ce
Miguel Perlado <perladomiguel7@gmail.com>
2026-08-11 11:47:16 +0800

    Version 1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import shutil
 
 #Input directory (where the files will be coming from)
 dir_path = Path(r"C:\Users\PCXPC\Documents\super secret hehehe\Proj\file-automation-project\Source")
@@ -13,15 +14,20 @@ for file in dir_path.iterdir():
     name, extension = entry.split(".") # split the file name by the "." and store in name and extension
 
     folder_name = "" # initialize empty dynamic folder_name placeholder
-    
+
+    # Assign the Folder Name according to file extension
     if extension == "csv":
-        folder_name = "csv" # if the extention name is csv, folder_name will be csv
+        folder_name = "csv" 
     elif extension == "xlsx":
-        folder_name = "excel" # if the extention name is xslx, folder name will be excel
+        folder_name = "excel" 
     elif extension == "txt": 
-        folder_name = "Text Files" # if the extention name is txt, folder name will be text files
+        folder_name = "Text Files" 
+    elif extension == "jpg" or extension == "jpeg" or extension == "png":
+        folder_name = "Photos"
 
     folder_path = output_path/folder_name # create the folder path pointing towards the output folder
     folder_path.mkdir(parents=True, exist_ok=True) # create the folder, bypass if already exists
+    shutil.copy2(file, folder_path)
 
-# Globalize the list of files (avoid repeating internal directory loops)
+
+
